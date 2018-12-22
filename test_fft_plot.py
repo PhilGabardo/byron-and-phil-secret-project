4d8eba1a4d85df66ce3dcf72fd1c9ba917ef4113
Adele Gabardo <adelegabardo@Adeles-MacBook-Pro.local>
2018-12-22 18:00:51 -0500

    fixed fft plotting

--- test_fft_plot.py
+++ test_fft_plot.py
@@ -1,30 +1,45 @@
 
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 import math
 
-#sample=16
-#a=[0]*sample
-#for i in range(sample):
-#  a[i] = math.sin(2*math.pi*i)
+Fs     = 44100.0  # Sampling rate (Hz)
+f      = 440.0    # Frequency (Hz)
 
-#f = numpy.fft.fft(a)
+Ts     = 1.0/Fs # Sampling interval
+sampleLength = 0.01 # s
+t      = np.arange(0,sampleLength,Ts)
 
-#for idx,s in enumerate(f):
-#  if s != 0:
-#    print("f[%d] = %s" %(idx,s))
+# sine wave
+y = np.sin(2 * np.pi * f * t)
 
-Fs = 8000
-f = 5
-sample = 8000
-x = numpy.arange(sample)
-y = numpy.sin(2 * numpy.pi * f * x / Fs)
-plt.plot(x, y)
-plt.xlabel('time sample(n)')
-plt.ylabel('amplitude')
-plt.show()
+#plt.plot(x, y)
+#plt.xlabel('time sample(n)')
+#plt.ylabel('amplitude')
+#plt.show()
 
-freq = numpy.arange(16000)
-f = numpy.fft.fft(y)
-plt.plot(freq, f)
+#freq = np.fft.fftfreq(sample, d=1.0/Fs)
+#fft_data = np.fft.fft(y)
+#plt.plot(freq, fft_data)
+#plt.show()
+
+n = len(y) # length of the signal
+k = np.arange(n)
+T = n/Fs
+frq = k/T # two sides of frequency range
+frq = frq[range(n/2)]
+
+Y = np.fft.fft(y)/n
+Y = Y[range(n/2)]
+
+maxFreqIdx = np.argmax(abs(Y))
+print("Max freq = %f Hz" %frq[maxFreqIdx])
+
+fig, ax = plt.subplots(2, 1)
+ax[0].plot(t,y)
+ax[0].set_xlabel('Time')
+ax[0].set_ylabel('Amplitude')
+ax[1].plot(frq,abs(Y),'r') # plotting the spectrum
+ax[1].set_xlabel('Freq (Hz)')
+ax[1].set_ylabel('|Y(freq)|')
 plt.show()
